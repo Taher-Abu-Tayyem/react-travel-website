a04c3a40f783afa906e92b97e3dd17b8acce997a
Taher-Abu-Tayyem <100804250+Taher-Abu-Tayyem@users.noreply.github.com>
2023-05-29 23:29:38 +0300

    Full-Travel

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import './App.css';
-import Home from './components/pages/Home';
-import { BrowserRouter as Router, Switch, Route, Routes, BrowserRouter } from 'react-router-dom';
+import React from "react";
+import Navbar from "./components/Navbar";
+import "./App.css";
+import Home from "./components/pages/Home";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Routes,
+  BrowserRouter,
+} from "react-router-dom";
 
-// import Services from './components/pages/Services';
-// import Products from './components/pages/Products';
-// import SignUp from './components/pages/SignUp';
+import Services from "./components/pages/Services";
+import Products from "./components/pages/Products";
+import SignUp from "./components/pages/SignUp";
 
 function App() {
   return (
-    <BrowserRouter> 
-    <Navbar/>
-    <Routes>
-      
-       
-        <Route path='/' element={ 
-        <Home/>
-        } />
-       
-    </Routes>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/Services" element={<Services />} />
+        <Route path="/Products" element={<Products />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
     </BrowserRouter>
   );
 }
